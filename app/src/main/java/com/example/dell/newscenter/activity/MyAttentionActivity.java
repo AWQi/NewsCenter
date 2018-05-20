e211787a141ec72735d783942d46064a236c34f5
AWQi <34415349+AWQi@users.noreply.github.com>
2018-05-20 10:40:12 +0800

    添加粉丝页面

--- app/src/main/java/com/example/dell/newscenter/activity/MyAttentionActivity.java
+++ app/src/main/java/com/example/dell/newscenter/activity/MyAttentionActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class AttentionActivity extends AppCompatActivity{
+public class MyAttentionActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
