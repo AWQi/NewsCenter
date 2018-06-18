8d6886d59e3bc3da9a1ecfea023bd20e357598b1
AWQi <34415349+AWQi@users.noreply.github.com>
2018-06-18 08:33:09 +0800

    theme 替换

---
+++ app/src/main/java/com/example/dell/newscenter/myview/MyToolBar.java
@@ -0,0 +1,26 @@
+package com.example.dell.newscenter.myview;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.dell.newscenter.R;
+
+public class MyToolBar extends Toolbar{
+
+    public MyToolBar(final Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.setLogo(R.drawable.return_icon);
+        this.setBackground(context.getResources().getDrawable(R.color.colorAccent));
+        this.getChildAt(0).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity)context).finish();
+            }
+        });
+    }
+}
